0426eeda2905bbdcbed1d841e48a40b013779929
Marckd4 <mmartinezcanete@gmail.com>
2025-11-27 12:39:42 -0300

    data_genera2.1

--- usuario/views.py
+++ usuario/views.py
@@ -1,11 +1,30 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm # formato de extarer fprmulario
+from django.contrib.auth.models import User
+
 
 def usuario(request):
-    return render (request, 'data_usuario.html',{
+    
+    if request.method == 'GET':
+        return render (request, 'data_usuario.html',{
         'form':UserCreationForm
     })
+        
+    
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            # registro user
+            try:
+                user = User.objects.create_user(username=request.POST['username'],password=request.POST['password1'])
+                user.save()
+                return HttpResponse('Usuario creado')
+            except:
+                return HttpResponse ('Usuario existe')
+        return HttpResponse('No conincide contraseña')
+        
+    
+    
     
 def home(request):
     return render(request, 'home.html')
